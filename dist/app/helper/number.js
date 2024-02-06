ca160683c8aa525193a78d5cc518a4b3de8d2a12
Lokesh <lokesh@75way.com>
2024-02-06 15:55:30 +0530

    remove package manager version

--- dist/app/helper/number.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandomArbitrary = void 0;
-function getRandomArbitrary(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-exports.getRandomArbitrary = getRandomArbitrary;
